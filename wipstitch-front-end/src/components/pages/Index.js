41627d3116bac4cf03f4f65ae0bdce11b21398eb
makennawold <makennawold@gmail.com>
2021-11-28 20:45:32 -0600

    generate random item from a prompt list

--- wipstitch-front-end/src/components/pages/Index.js
+++ wipstitch-front-end/src/components/pages/Index.js
@@ -8,7 +8,18 @@ export function Index() {
   const { user, login } = useContext(UserContext);
   const [data, setData] = useState([]);
   const [selectedItem, setSelectedItem] = useState(0);
-  const [generatedItem, setGeneratedItem] = useState("");
+  const [generatedItem, setGeneratedItem] = useState(0);
+
+  const randomGenerate = () => {
+    // random list vv
+    // let randomIndex = Math.floor(Math.random() * data.length);
+    // setGeneratedItem(data[randomIndex].id);
+    // console.log(generatedItem);
+    const listItems = data[selectedItem].items;
+    const listItemsArray = listItems.split(", ");
+    const randomListIndex = Math.floor(Math.random() * listItemsArray.length);
+    const randomListItem = setGeneratedItem(listItemsArray[randomListIndex]);
+  };
 
   return (
     <div className="home">
@@ -25,9 +36,14 @@ export function Index() {
         setData={setData}
       />
       <div className="generate-prompt-wrapper">
-        <p className="generate-btn">generate</p>
-        {}
-        <p>pick a list to generate a random prompt!</p>
+        <p className="generate-btn" onClick={randomGenerate}>
+          generate
+        </p>
+        {generatedItem ? (
+          <p>{generatedItem}</p>
+        ) : (
+          <p>pick a list to generate a random prompt!</p>
+        )}
       </div>
       <div>wips progress carousel component</div>
       {/* note: each of these components should be getting lists, which has individual list items, and all of it is styled in the actual carousel component, list editor =, or wip editor */}
