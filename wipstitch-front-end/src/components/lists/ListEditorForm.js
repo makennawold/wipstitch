fcb5227257d35d69a89fc21c4b61e7d9b629359b
makennawold <makennawold@gmail.com>
2021-11-30 12:27:47 -0600

    updated form to work with reducer

--- wipstitch-front-end/src/components/lists/ListEditorForm.js
+++ wipstitch-front-end/src/components/lists/ListEditorForm.js
@@ -1,5 +1,13 @@
-import React, { useContext, useState, useEffect, useLayoutEffect } from "react";
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useReducer,
+} from "react";
 import { FaTimes, FaEdit } from "react-icons/fa";
+
+import formReducer from "../../reducers/formReducer";
 
 import FormItems from "./FormItems";
 import FormItem from "./FormItem";
@@ -11,68 +19,83 @@ export default function ListEditorForm({
   setEditorMode,
   updateList,
 }) {
-  const [listName, setListName] = useState("");
-  const [items, setItems] = useState([]);
-  const [publicStatus, setPublicStatus] = useState(true);
+  //   const [listName, setListName] = useState("");
+  // //   const [items, setItems] = useState([]);
+  //   const items = [];
+  //   const [publicStatus, setPublicStatus] = useState(true);
 
-  const updateItems = (e, id) => {
-    const updatedItems = items;
-    updatedItems[id] = e;
-    setItems(updatedItems);
-    console.log(items);
+  //     function itemReducer(state, action) {
+  //         switch (action.type) {
+  //             case 'update':
+  //             return
+  //         }
+  //     }
+  const initialFormState = {
+    listName: "",
+    items: [],
+    public: true,
   };
 
-  //   const formItems = () => {
-  //     // const itemList = items.split(", ");
-  //     return items.map((item) => {
-  //       return (
-  //         <div className="list-item-wrapper" key={items.indexOf(item)}>
-  //           {editorMode === "edit" ? (
-  //             <FormItem
-  //               itemValue={item}
-  //               updateItems={updateItems}
-  //               id={items.indexOf(item)}
-  //               deleteItem={deleteItem}
-  //             />
-  //           ) : (
-  //             <div className="list-item">
-  //               <span className="circle" />
-  //               <div>{item}</div>
-  //             </div>
-  //           )}
-  //         </div>
-  //       );
-  //     });
-  //   };
+  //   const [selectedListData, setSelectedListData] = useState(
+  //     data[selectedItem - 1]
+  //   );
+  const [formState, dispatch] = useReducer(formReducer, initialFormState);
+
+  const handleFormChange = (e) => {
+    dispatch({
+      type: "HANDLE UPDATE",
+      field: e.target.name,
+      payload: e.target.value,
+    });
+  };
+
+  const handleInitial = (name, value) => {
+    dispatch({
+      type: "HANDLE UPDATE",
+      field: name,
+      payload: value,
+    });
+  };
+
+  const updateItems = (e, id) => {
+    const updatedItems = formState.items;
+    updatedItems[id] = e;
+    // setItems(updatedItems);
+    //use reducer to change items
+    console.log(formState.items);
+  };
 
   const cancelUpdate = () => {
     setEditorMode("view");
   };
 
   const deleteItem = (item) => {
-    const updatedItems = items;
+    const updatedItems = formState.items;
     const index = updatedItems.indexOf(item);
     updatedItems.splice(index, 1);
-    setItems(updatedItems);
+    // setItems(updatedItems);
+    //use reducer to change items
   };
 
   const handleSubmit = () => {
-    const newItems = items.join(", ");
+    const newItems = formState.items.join(", ");
     const id = data[selectedItem - 1].id;
-    updateList(listName, newItems, publicStatus, id);
+    updateList(formState.listName, newItems, formState.publicStatus, id);
   };
 
   useEffect(() => {
     const selectedListData = data[selectedItem - 1];
-    setListName(selectedListData.list_name);
-    setItems(selectedListData.items.split(", "));
-    setPublicStatus(selectedListData.public);
+    handleInitial("listName", selectedListData.list_name);
+    handleInitial("items", selectedListData.items.split(", "));
+    handleInitial("public", selectedListData.public);
+
+    // setSelectedListData(data[selectedItem - 1]);
+    // setListName(selectedListData.list_name);
+    // setItems(selectedListData.items.split(", "));//
+    // setPublicStatus(selectedListData.public);
+
     cancelUpdate();
   }, [selectedItem]);
-
-  //   useLayoutEffect(() => {
-  //     console.log("list editor layour effect");
-  //   });
 
   return (
     <div className="list-editor">
@@ -82,26 +105,27 @@ export default function ListEditorForm({
         <div className="list-name-wrapper">
           <input
             className="list-name"
-            value={listName}
-            onChange={(e) => setListName(e.target.value)}
+            value={formState.listName}
+            // onChange={(e) => setListName(e.target.value)}
+            onChange={(e) => handleFormChange(e)}
           />
           <div onClick={() => cancelUpdate()}>cancel</div>
         </div>
       ) : (
         <div className="list-name-wrapper">
-          <div className="list-name">{listName}</div>
+          <div className="list-name">{formState.listName}</div>
           <FaEdit onClick={() => setEditorMode("edit")} />
         </div>
       )}
       {/* <div>{items}</div> */}
       {/* {formItems()} */}
       {console.log("hello")}
-      <FormItems
+      {/* <FormItems
         items={items}
         deleteItem={deleteItem}
         updateItems={updateItems}
         editorMode={editorMode}
-      />
+      /> */}
       {editorMode === "edit" ? (
         <button onClick={() => handleSubmit()}>update api</button>
       ) : null}
